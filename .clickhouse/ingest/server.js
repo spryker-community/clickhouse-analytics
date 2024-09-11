932d87f5444ebafc823eba159a9cfa3f1328f969
Nicolas Messias <nicolas.messias@kp-family.de>
2024-09-11 13:24:00 +0000

    trying to insert data

--- .clickhouse/ingest/server.js
+++ .clickhouse/ingest/server.js
@@ -1,11 +1,39 @@
-const { createServer } = require('node:http');
-const hostname = '0.0.0.0';
-const port = 8888;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const {createServer} = require('node:http');
+const { ClickHouse } = require('clickhouse');
+
+const clickhouse = new ClickHouse({
+    url: `http://${process.env.CLICKHOUSE_HOST}`,
+    port: process.env.CLICKHOUSE_CLICKHOUSE_HTTP_PORT,
+    basicAuth: {
+        username: process.env.CLICKHOUSE_USER,
+        password: process.env.CLICKHOUSE_PASSWORD
+    },
+    config: {
+        database: process.env.CLICKHOUSE_DB,
+    }
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+const server = createServer(async (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    let messages = "start \n";
+    try {
+        const response = await clickhouse.insert('INSERT INTO Event(name, user_id, value, content, timestamp) VALUES', [
+            'page_view',
+            '1',
+            0,
+            '{"name": "Product Detail Page"}',
+            '2024-09-11T13:48:08Z',
+        ]).toPromise();
+
+        messages += JSON.stringify(response) + "\n";
+    } catch(error) {
+        messages += error.message;
+    }
+
+    messages += " end";
+    res.end(messages);
+});
+server.listen(8888, '0.0.0.0', () => {
 });
--- .clickhouse/migration/1_init.sql
+++ .clickhouse/migration/1_init.sql
@@ -1,12 +1,10 @@
-SET allow_experimental_json_type = 1;
 CREATE TABLE IF NOT EXISTS Event
 (
-    id UInt64,
     name String,
     timestamp DateTime('UTC'),
     user_id String,
     value Double,
-    content JSON,
+    content String
 )
 ENGINE=AggregatingMergeTree
 PARTITION BY toYYYYMM(timestamp)
