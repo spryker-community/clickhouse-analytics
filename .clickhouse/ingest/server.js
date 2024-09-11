b88a6046379f0f971f3d9ac16c736a6fa40f10a9
Nicolas Messias <nicolas.messias@kp-family.de>
2024-09-11 10:25:57 +0000

    finished inital node setup

--- .clickhouse/ingest/server.js
+++ .clickhouse/ingest/server.js
@@ -1,5 +1,5 @@
 const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 8888;
 const server = createServer((req, res) => {
   res.statusCode = 200;
