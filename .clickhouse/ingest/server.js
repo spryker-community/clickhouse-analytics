5a31668b324d4bfca9b22b9c35d87dc015832c83
Nicolas Messias <nicolas.messias@kp-family.de>
2024-09-11 10:10:39 +0000

    separate file for ingest

--- .clickhouse/ingest/server.js
+++ .clickhouse/ingest/server.js
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
-const port = 8888;
+const port = 8125;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
